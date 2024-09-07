63e61e4bfbb30889dff6be58c7a544db86e7dcd2
Avinash96-gthb <avinash.a2k4@gmail.com>
2024-09-07 18:29:01 +0530

    Started login page

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex w-full h-full justify-center">
+      <h1 className="text-xl text-red-500">
+        Welcome to Optimanifacture
+      </h1>
+    </div>
+  );
+}
