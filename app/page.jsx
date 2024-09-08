dbedca28e1402cbd99e46e9a73d8ed5c52ce904f
Avinash96-gthb <avinash.a2k4@gmail.com>
2024-09-08 19:38:47 +0530

    logout implimented

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
+import { logout } from "./logout/actions";
 
 export default function Home() {
   return (
+    <main>
+    <form action={logout}>
+      <button type="submit">
+        logout
+      </button>
+    </form>    
     <div className="flex w-full h-full justify-center">
       <h1 className="text-xl text-red-500">
         Welcome to Optimanifacture
       </h1>
     </div>
+    </main>
   );
 }
