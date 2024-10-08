170aef90fdc14f5cc1d13c67f6ff1ba8b45232d7
Kush_Puro <kp4010@srmist.edu.in>
2024-10-08 20:17:53 +0530

    Delete Optimanufacture-Version 2 directory

--- Optimanufacture-Version 2/app/GoToLogin/actions.js
+++
@@ -1,6 +0,0 @@
-'use server'
-import { redirect } from "next/navigation";
-
-export async function GoToLogin(){
-    redirect('/login')
-}
--- Optimanufacture-Version 2/utils/supabase/client.js
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr'
-
-export function createClient() {
-  return createBrowserClient(
-    YOUR_SUPABASE_URL,
-    YOUR_SUPABASE_ANON_KEY
-  )
-}
