3a7d34b796ab3962f80b9203daece9160fa727e3
Avinash96-gthb <113711198+Avinash96-gthb@users.noreply.github.com>
2024-10-10 20:17:54 +0530

    FIX: removed url dsiplay

--- utils/supabase/server.js
+++ utils/supabase/server.js
@@ -5,8 +5,8 @@ export function createClient() {
   const cookieStore = cookies()
 
   return createServerClient(
-    "https://onrlyldxtqwzyyccyisy.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ucmx5bGR4dHF3enl5Y2N5aXN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU3MDk5NjMsImV4cCI6MjA0MTI4NTk2M30.f8WKTDbEU-iGGYNvXkSwnpo0C41zHCDMGioCffrqwhg",
+    process.env.YOUR_SUPABASE_URL,
+    process.env.YOUR_SUPABASE_ANON_KEY,
 
     {
       cookies: {
